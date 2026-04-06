238aff0fe9200ff453c79c05144313dd590d8d38
b1u3rchie <b1u3b3rrys@proton.me>
2026-04-06 17:45:12 +0200

    ssh testing

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,3 @@
-function fish_greeting
-    echo Hi!
- end
-function fish_prompt --description "Write out the prompt"
-  set exit_status $status
-  
-  set_color $fish_color_cwd
-  if [ $PWD = $HOME ]
-    set cwd "~"
-  else
-    set cwd (basename $PWD)
-  end
-  echo -n "$cwd "
-
-  if [ $exit_status -eq 0 ]
-    set_color normal
-  else
-    set_color $fish_color_error
-  end
-
-  if [ $USER = "root" ]
-    echo -n "# "
-  else
-    echo -n "> "
-  end
-
-  set_color normal
+if status is-interactive
+# Commands to run in interactive sessions can go here
 end
